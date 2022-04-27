d419b5c0b33c1590ac2e8dd854e33214bde0e53b
thereaper90917 <viper90917@gmail.com>
2022-04-27 17:15:57 +0200

    added Terminal Name

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
-<h1>Hello There!</h1>
+<script>
+	const os = require("os");
+	const hostName = os.hostname();
+</script>
+
+
+<h1>Terminal: <span>{hostName}</span></h1>
 
 <style>
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 2em;
+		font-weight: 100;
+	}
+	span {
+		color: #13099e;
+		text-transform: uppercase;
+		font-size: 1em;
 		font-weight: 100;
 	}
 </style>
